0aa3bbf7e4c024c7d78ece13ee31132c239b689f
Krishnapriya S Nair <krishnapriyasnair9@gmail.com>
2026-01-07 22:39:44 +0530

    changed views

--- emplyees/serializers.py
+++ emplyees/serializers.py
@@ -36,7 +36,7 @@ class EmployeeListSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'username', 'email', 'first_name', 'last_name',
             'phone_number', 'role', 'current_status', 'department',
-            'designation', 'employee_id','profile_image', 'profile_image_url',
+            'designation','profile_image_url',
             
         ]
         read_only_fields = ['created_at', 'updated_at']
